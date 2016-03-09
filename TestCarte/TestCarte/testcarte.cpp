74948e1d24c0dae1919ea9990d1223653ba51af6
Jmercier13 <jmercier13@hotmail.fr>
2016-03-09 17:39:41 -0500

    Archives changes

--- TestCarte/TestCarte/testcarte.cpp
+++ TestCarte/TestCarte/testcarte.cpp
@@ -8,3 +8,33 @@ TestCarte::~TestCarte()
 {
 
 }
+
+
+void TestCarte::testSuivant(){
+
+}
+
+
+void TestCarte::premier(){
+	_index = 0;
+	setArchive(_archive.getObj(_index));
+	setArchive(_index, _archive.length());
+}
+
+void TestCarte::dernier(){
+	_index = _archive.length();
+	setArchive(_archive.getObj(_index));
+	setArchive(_index, _archive.length());
+}
+
+void TestCarte::precedent(){
+	_index--;
+	setArchive(_archive.getObj(_index));
+	setArchive(_index, _archive.length());
+}
+
+void TestCarte::suivant(){
+	_index++;
+	setArchive(_archive.getObj(_index));
+	setArchive(_index, _archive.length());
+}
--- TestCarte/TestCarte/testcarte.h
+++ TestCarte/TestCarte/testcarte.h
@@ -1,16 +1,27 @@
 #pragma once
 
 #include <string>
-
+#include <list.h>
 #include "VisiTest.h"
 
 using namespace std;
 
 class TestCarte : public VisiTest{
+	private:
+		List<DonneesTest> _archive;
+		int _index = 0;
+
 	public:
 		TestCarte(string title);
 		~TestCarte();
-
-	private:
+		//Test section
+		void testSuivant();
+		
+		//Archive Section
+		void premier();
+		void dernier();
+		void precedent();
+		void suivant();
+		
 };
 
